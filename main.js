385f647115e75a6282f135649a46161c937c4d2d
Julian Compagni Portis <jcompagni@gmail.com>
2024-05-01 14:18:20 -0400

    scaffold exentsion

--- main.js
+++ main.js
@@ -1,59 +1,11 @@
-function removeScriptTags(htmlString) {
-    // Create a new DOMParser instance
-    const parser = new DOMParser();
-
-    // Parse the HTML string into a DOM Document
-    const doc = parser.parseFromString(htmlString, 'text/html');
-
-    // Find all script elements in the document
-    const scripts = doc.getElementsByTagName('script');
-
-    // Remove all script elements
-    while (scripts.length > 0) {
-        scripts[0].parentNode.removeChild(scripts[0]);
-    }
-
-    // Serialize the Document back into a string
-    // Using XMLSerializer to convert the document to a string
-    const serializer = new XMLSerializer();
-    return serializer.serializeToString(doc);
-}
-
-
-function getArchiveBody(htmlString){
-        const parser = new DOMParser();
-
-    // Parse the HTML string into a DOM Document
-    return parser.parseFromString(htmlString, 'text/html');
-
-}
-
-
-
-function fetchArchive(url){
-
-    return fetch("http://archive.is/latest/"+url, {
-        method: 'GET',
-    })
-        .then(response.text())
-        .then(getArchiveBody)
-}
-
-
-
-fetchArchive(location.href)
-    .then(html=>document.doucmentElement = html)
-
-// fetch(location.href, {
-//     method: 'GET',
-//     headers: {
-//         'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-//     }
-// })
-//     .then(response => response.text()) // Process the response as text
-//     .then(html => {
-//         let newHTML = removeScriptTags(html);
-//         document.doucmentElement.innerHTML = newHTML;
-//         console.log(newHTML);
-//     } )
-//     .catch(error => console.error('Error fetching the data:', error));
+const host = "https://a503-71-105-119-119.ngrok-free.app";
+fetch(`${host}?url=${location.href}`, {
+    method: 'GET',
+    headers: {'ngrok-skip-browser-warning': true}
+})
+    .then(response => response.text()) // Process the response as text
+    .then(html => {
+        console.log(html);
+        document.documentElement.innerHTML = html;
+    } )
+    .catch(error => console.error('Error fetching the data:', error));
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import { parse } from 'node-html-parser';
+
 
 const app = express();
 const PORT = 3000;
@@ -9,25 +11,32 @@ app.use(cors());
 
 
 function getArchiveBody(htmlString){
-    const parser = new DOMParser();
+    const html = parse(htmlString);
 
-    // Parse the HTML string into a DOM Document
-    return parser.parseFromString(htmlString, 'text/html');
+    let innerContent = html.getElementById("CONTENT");
+    let newHTML = html.querySelector("body").set_content(innerContent);
+
+
+    return newHTML.toString();
+
+
 
 }
 
 function fetchArchive(url){
 
+    console.log(`fetching: ${url}`);
+
     return fetch("http://archive.is/latest/"+url, {
         method: 'GET',
     })
-        .then(resp=>resp.text());
-        // .then(getArchiveBody);
+        .then(resp=>resp.text())
+        .then(getArchiveBody);
 }
 
 app.get('/', async (req, res) => {
-    let data = await fetchArchive(URL);
-    console.log(data);
+    console.log(`query: ${JSON.stringify(req.query)}`);
+    let data = await fetchArchive(req.query.url);
 
     res.send(data);
 
